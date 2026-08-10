1c49612357ffcdf4e4b6fcf14b0929f0fb78d848
dxn-scrlt <dx.scarlet.xn@gmail.com>
2026-08-09 22:16:42 -0400

    feat: add add command

---
+++ internal/commands/add.go
@@ -0,0 +1,35 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/dxn-scrlt/protectdf/internal/pdf"
+)
+
+func AddPassword(input string) error {
+	var password string
+	var retype string
+
+	fmt.Print("Enter password: ")
+	fmt.Scanln(&password)
+
+	fmt.Print("Re-enter password: ")
+	fmt.Scanln(&retype)
+
+	var err error
+
+	if password != retype {
+		err = errors.New("passwords do not match")
+	} else {
+		output := strings.TrimSuffix(input, ".pdf") + "-protected.pdf"
+
+		err = pdf.EncryptFile(input, output, password)
+		if err != nil {
+			err = errors.New("add password unsuccessful")
+		}
+	}
+
+	return err
+}
